d6b5d307b1093e71d7d7ca8799c85da371cfd638
Oliver <oliverfruergaard@gmail.com>
2025-04-20 15:36:28 +0200

    add error handling to reddit fetch

--- src/lib/reddit/get-top-news.jsx
+++ src/lib/reddit/get-top-news.jsx
@@ -1,11 +1,21 @@
 import formatDate from "../helper-functions/format-date";
-
-const GetTopNews = async (scope) => {
+const GetTopNews = async () => {
   const response = await fetch(
     "https://www.reddit.com/r/Denmark/search.json?q=flair_name%3A%22news%22&restrict_sr=1&type=link&sort=top&t=week&limit=10"
   );
+
+  console.log("Response Status:", response.status);
+  console.log("Response Headers:", response.headers);
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    console.error("Error fetching data from Reddit:", errorText);
+    throw new Error(`Error fetching Reddit data: ${response.statusText}`);
+  }
+
   const data = await response.json();
   console.log("reddit data", data.data);
+
   const posts = data.data.children.map((post) => ({
     title: post.data.title,
     url: post.data.url,
